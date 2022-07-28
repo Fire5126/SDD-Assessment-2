d54172075d37744a374374fab7807c238a59ca16
Fire5126 <Xin.Dobbie@gmail.com>
2022-07-28 15:44:34 +1000

    AnimDoor

---
+++ Assets/Script/TriggerAnimation.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerAnimation : MonoBehaviour
+{
+
+    Animator animator;
+
+    public GameObject objectToMove;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            objectToMove.GetComponent<Animator>().SetTrigger("playerEnters");
+        }
+    }
+}
